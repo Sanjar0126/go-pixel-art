e5c6c188f04a1e3f53dede02dcc4e1f8144cb483
sanjar <sanjar@>
2025-09-18 14:20:36 +0500

    rgb to vector

---
+++ pixelart/internal.go
@@ -0,0 +1,39 @@
+package pixelart
+
+import (
+	"bufio"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+func clamp(v, lo, hi float64) float64 {
+	if v < lo {
+		return lo
+	}
+	if v > hi {
+		return hi
+	}
+	return v
+}
+
+func rgbToVec(c color.Color) ColorVec {
+	r, g, b, _ := c.RGBA()
+	return ColorVec{float64(r >> 8), float64(g >> 8), float64(b >> 8)}
+}
+
+func dist2(a, b ColorVec) float64 {
+	dx := a[0] - b[0]
+	dy := a[1] - b[1]
+	dz := a[2] - b[2]
+	return dx*dx + dy*dy + dz*dz
+}
+
+// --- Palette building (kmeans etc.) ---
+// (same functions: kmeans, loadImage, sampleColorsFromImage, buildPaletteFromDir, nearestPaletteColor)
+
+// --- Processing ---
+// (same function: processImageToPixelArt)
+
+
